47c8fb433bb91350c9d4e88eedcadc1342d935ca
Josh Hedlund <josh.hedlund@commentsold.com>
2023-12-25 16:15:27 -0600

    fuction to store trip

--- repositories/TripRepository.py
+++ repositories/TripRepository.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import sys
 sys.path.append(os.path.dirname(os.getcwd()))
@@ -12,5 +13,16 @@ def getTrip(session: Session, tripId: int) -> Trip:
     trip = session.query(Trip) \
         .filter(Trip.id == tripId) \
         .first()
+
+    return trip
+
+
+def storeTrip(session: Session, userId: int, name: str, lat: float, lng: float, year: int, month: int) -> Trip:
+
+    trip = Trip(userId=userId, name=name, latitude=lat, longitude=lng, year=year, month=month)
+    trip.createdAt = datetime.now()
+    trip.updatedAt = datetime.now()
+    session.add(trip)
+    session.commit()
 
     return trip
